fcfe05e0a4ce0b685299b97926f65972a81b7891
aaron <kaven22314567@gmail.com>
2023-02-18 00:57:59 +0800

    feat: add modal component

---
+++ src/components/CustomModal.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { onMounted, onUnmounted } from "vue";
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+    default: "",
+  },
+});
+const emit = defineEmits(["update:modelValue"]);
+const body = document.querySelector("body");
+
+onMounted(() => {
+  body.classList.add("not-scroll");
+});
+onUnmounted(() => {
+  body.classList.remove("not-scroll");
+});
+</script>
+<template>
+  <div class="modal-container" @click.self="emit('update:modelValue', false)">
+    <div class="modal normal-container">
+      <h2 v-if="props.title" class="page-title">{{ props.title }}</h2>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.modal-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: var(--light-main-bg);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .modal {
+    margin: 0 32px;
+    flex-grow: 1;
+    max-width: 500px;
+  }
+}
+</style>
--- src/pages/BookKeepAssets.vue
+++ src/pages/BookKeepAssets.vue
@@ -1,1 +1,10 @@
-<template>bookkeepcash</template>
+<script setup>
+import CustomModal from "@/components/CustomModal.vue";
+import { ref } from "vue";
+const showModal = ref(true);
+</script>
+<template>
+  <h1 class="page-title">Assets</h1>
+  <button @click="showModal = true">123123</button>
+  <CustomModal v-if="showModal" v-model="showModal" title="NEW Asset" />
+</template>
